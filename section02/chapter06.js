a97598cdf79851a42d330043a5da882f42c6baa4
DESKTOP-CGAAFBA\user <wls862@setsec.co.kr>
2025-10-03 09:49:44 +0900

    4일차

---
+++ section02/chapter06.js
@@ -0,0 +1,34 @@
+// 1.배열 순회
+let arr = [1,2,3];
+// 1-1.배열 인덱스
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+// 1-2.for of 반복문
+for(let item of arr){
+    console.log(item);
+}
+
+// 2.객체 순회
+
+let person ={
+    name : "이정환",
+    age: 27,
+    hobby: "테니스"
+}
+
+// 2-1.Object.keys 사용
+// 객체에서 key값들만 뽑아서 새로운 배열만 반환하는 내장함수
+
+let keys = Object.keys(person);
+for(let key of keys){
+    console.log(key,person[key])
+}
+
+// 2-2.Object.values
+// 객체에서 value값들만 뽑아서 새로운 배열로 반환
+let values = Object.values(person);
+for(let value of values){
+    console.log(value);
+}
