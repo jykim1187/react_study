f84f160f59ad79d2d9bf278a289efe658b6d25e5
Jin <rifkehtm862@gmail.com>
2025-10-15 23:00:39 +0900

    10일차

---
+++ section07/src/App.jsx
@@ -0,0 +1,56 @@
+
+import './App.css'
+import Controller from './components/Controller';
+import Even from './components/Even';
+import Viewer from './components/Viewer';
+import { useState,useEffect,useRef } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input,setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1.마운트 : 탄생
+  useEffect(()=>{
+    console.log("mount")
+  },[]);
+
+  // 2.업데이트
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return
+    }
+    console.log("update")
+  })
+
+  // 3. 언마운트 : 죽음
+
+
+  const onClickButton = (value) =>{
+    setCount(count + value);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e)=>{
+          setInput(e.target.value);
+        }} />
+      </section>
+
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ?<Even/>: null}
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton}/>
+      </section>
+    </div>
+    );
+  
+}
+
+export default App
