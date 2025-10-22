7250b06f036aecf57cb33b4d894587f83ae6b4b8
DESKTOP-CGAAFBA\user <wls862@setsec.co.kr>
2025-10-22 09:48:20 +0900

    17일차-1

--- setcion12/src/pages/Home.jsx
+++ setcion12/src/pages/Home.jsx
@@ -1,15 +1,35 @@
+import { useState,useContext } from "react";
+
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 import Header from "../components/Header";
+import { DiaryStateContext } from "../App";
 
+
+
+const getMonthlyData = (pivotDate, data) =>{
+    const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(),1,0,0,0).getTime();
+    const endtime = new Date(pivotDate.getFullYear(), pivotDate.getMonth()+1,0,23,59,59).getTime();
+    return data.filter((item) => item.createdDate >=beginTime && item.createdDate <=endtime )
+}
 
 const Home = () =>{
+    const data =useContext(DiaryStateContext);
+    const[pivotDate, setPivotDate] = useState(new Date());
+
+    const onIncreaseMonth = () =>{
+        setPivotDate(new Date(pivotDate.getFullYear(),pivotDate.getMonth() +1))
+    }
+
+    const onDecreaseMonth = () =>{
+            setPivotDate(new Date(pivotDate.getFullYear(),pivotDate.getMonth() -1))
+    }
 
     return <div> 
     <Header 
-    title={"2024년 10월"}
-    leftChild={<Button text={"<"}/>}
-    rightChild={<Button text={">"}/>}
+    title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`}
+    leftChild={<Button onClick={onDecreaseMonth} text={"<"}/>}
+    rightChild={<Button onClick={onIncreaseMonth} text={">"}/>}
     />
     <DiaryList />
     </div>
